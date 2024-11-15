bd1da5a9fecb387a0c0278c69c7ce67cd37928ee
Nermin <nermin.memovic990.com>
2024-11-15 19:07:52 +0100

    Updates

--- src/analytics.js
+++
@@ -1,9 +0,0 @@
-import ReactGA from 'react-ga4';
-
-export const initGA = () => {
-    ReactGA.initialize('G-YHGELWXGS6'); // Ubaci ovde svoj Measurement ID
-};
-
-export const trackPageView = () => {
-    ReactGA.send('pageview');
-};
