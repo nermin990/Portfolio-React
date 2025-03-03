702be1edbc5d13057b79b07ec21da91329626d6d
Nermin <nermin.memovic990.com>
2025-03-03 16:44:28 +0100

    Added loader animation to the site

--- src/scripts/Typing.js
+++ src/scripts/Typing.js
@@ -1,36 +1,53 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import anime from "animejs";
 
 const AnimatedHeading = () => {
-  const textWrapperRef = useRef(null);
+    const [letters, setLetters] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
 
-  useEffect(() => {
-    // Prepoznavanje i razdvajanje svakog slova u <span class='letter'>
-    const textWrapper = textWrapperRef.current;
-    if (textWrapper) {
-      textWrapper.innerHTML = textWrapper.textContent.replace(
-        /\S/g,
-        "<span class='letter'>$&</span>"
-      );
+    useEffect(() => {
+        const text = "Nermin Memovic";
+        setLetters(text.split(""));
 
-      // Animacija sa Anime.js
-      anime.timeline().add({
-        targets: ".portfolio-holder__left-heading .letter",
-        scale: [0, 1],
-        duration: 1500,
-        elasticity: 600,
-        delay: (el, i) => 80 * (i + 1),
-      });
+        // Simulacija učitavanja sa setTimeout (možeš koristiti pravi preloader)
+        setTimeout(() => {
+            setIsLoaded(true); // Ovaj state možeš koristiti za prikazivanje sadržaja
+        }, 1000); // Možeš prilagoditi trajanje preloadera
+    }, []);
+
+    useEffect(() => {
+        if (isLoaded) {
+            requestAnimationFrame(() => {
+                anime.timeline().add({
+                    targets: ".portfolio-holder__left-heading .letter",
+                    scale: [0, 1],
+                    duration: 1500,
+                    elasticity: 600,
+                    delay: (el, i) => 80 * (i + 1),
+                });
+            });
+        }
+    }, [isLoaded]);
+
+    if (!isLoaded) {
+        return (
+            <div className="loader">
+                <div className="circle"></div>
+            </div>
+        ); // Tvoj preloader
     }
-  }, []);
 
-  return (
-    <h1 className="portfolio-holder__left-heading">
-      <span className="text-wrapper" ref={textWrapperRef}>
-        <span className="letters">Nermin Memovic</span>
+    return (
+        <h1 className="portfolio-holder__left-heading">
+      <span className="text-wrapper">
+        {letters.map((letter, index) => (
+            <span key={index} className="letter">
+            {letter}
+          </span>
+        ))}
       </span>
-    </h1>
-  );
+        </h1>
+    );
 };
 
 export default AnimatedHeading;
